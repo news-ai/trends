714a32612fcd9e9a372754ae61969e6fb8e8b57d
Abhi Agarwal <me@abhiagarwal.com>
2016-05-29 16:50:56 -0400

    Structuring app and first iteration over getting data and running pattern

--- trends.py
+++ trends.py
@@ -6,23 +6,48 @@ import datetime
 # Third-party app imports
 import requests
 import dateutil.parser as parser
+from pymongo import MongoClient
+from newspaper import Article
 from pattern.web import Newsfeed, plaintext, HTTP404NotFound
 from pattern.db import date as datecalc
 from pattern.vector import Model, Document, LEMMA
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 # Imports from app
-from middleware.config import (
-    CONTEXT_API_USERNAME,
-    CONTEXT_API_PASSWORD,
-)
+from trends.internal.context import get_login_token
 
 # Removing requests warning
 urllib3.disable_warnings()
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+# MongoDB setup
+client = MongoClient(connect=False)
+db = client.trends
+articles_collection = db.articles
+
 # Context Setup
 base_url = 'https://context.newsai.org/api'
+
+
+def get_article_text(url):
+    mongo_article = articles_collection.find_one({'url': url})
+    if mongo_article:
+        return mongo_article['article']
+
+    article = Article(url)
+    article.download()
+    article.parse()
+
+    article_text = article.text.split('\n')
+    article_text = ' '.join(article_text)
+
+    data = {
+        'article': article_text,
+        'url': url,
+    }
+
+    articles_collection.insert_one(data)
+    return article_text
 
 
 def articles_to_trends(articles):
@@ -34,7 +59,10 @@ def articles_to_trends(articles):
             dt = parser.parse(dt)
             dt = dt.strftime("%Y-%m-%d")
             d = str(datecalc(dt, format='%Y-%m-%d'))
-            s = plaintext(story['summary'])
+
+            article_text = get_article_text(story['url'])
+            s = plaintext(article_text)
+
             # Each key in the news dictionary is a date: news is grouped per day.
             # Each value is a dictionary of id => story items.
             # We use hash(story['summary']) as a unique id to avoid duplicate
@@ -55,23 +83,6 @@ def articles_to_trends(articles):
         print document.keywords(top=10)
 
 
-def get_login_token():
-    headers = {
-        "content-type": "application/json",
-        "accept": "application/json"
-    }
-    payload = {
-        "username": CONTEXT_API_USERNAME,
-        "password": CONTEXT_API_PASSWORD,
-    }
-
-    r = requests.post(base_url + "/jwt-token/",
-                      headers=headers, data=json.dumps(payload), verify=False)
-    data = json.loads(r.text)
-    token = data.get('token')
-    return token
-
-
 def get_articles():
     token = get_login_token()
     headers = {
@@ -80,7 +91,7 @@ def get_articles():
         "authorization": "Bearer " + token
     }
 
-    r = requests.get(base_url + '/articles/?limit=200',
+    r = requests.get(base_url + '/articles/?limit=500',
                      headers=headers, verify=False)
     return r.json()
 
@@ -89,4 +100,6 @@ def process_articles():
     articles = get_articles()['results']
     articles_to_trends(articles)
 
-process_articles()
+
+if __name__ == "__main__":
+    process_articles()
---
+++ trends/internal/context.py
@@ -0,0 +1,36 @@
+# Stdlib imports
+import json
+
+# Third-party app imports
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# Imports from app
+from middleware.config import (
+    CONTEXT_API_USERNAME,
+    CONTEXT_API_PASSWORD,
+)
+
+# Removing requests warning
+urllib3.disable_warnings()
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# Context Setup
+base_url = 'https://context.newsai.org/api'
+
+
+def get_login_token():
+    headers = {
+        "content-type": "application/json",
+        "accept": "application/json"
+    }
+    payload = {
+        "username": CONTEXT_API_USERNAME,
+        "password": CONTEXT_API_PASSWORD,
+    }
+
+    r = requests.post(base_url + "/jwt-token/",
+                      headers=headers, data=json.dumps(payload), verify=False)
+    data = json.loads(r.text)
+    token = data.get('token')
+    return token
